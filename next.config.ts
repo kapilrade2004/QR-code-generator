15f6edc0c99d0cfaec231f998db62754b9f24382
kapilrade2004 <kapilrade2004@gmail.com>
2026-09-19 09:51:38 +0530

    feat: add API rewrites to proxy requests to the backend server

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const backendUrl =
+  process.env.NEXT_PUBLIC_BACKEND_URL ||
+  "https://qr-cod-generator-backend.onrender.com";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
